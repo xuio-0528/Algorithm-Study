26a494f92b30434eed27724c0ff4c33208d813f8
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-05-16 22:12:57 +0900

    #Taewoong1378 - 0516 : 2문제 해결

---
+++ Taewoong/0516/2525.py
@@ -0,0 +1,15 @@
+import sys
+
+H, M = map(int, sys.stdin.readline().split())
+timer = int(input())
+
+H += timer // 60
+M += timer % 60
+
+if M >= 60:
+    H += 1
+    M -= 60
+if H >= 24:
+    H -= 24
+
+print(H,M)
